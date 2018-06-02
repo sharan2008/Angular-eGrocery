dfdf6147297cd4403078d03446f096ed6848a3fe
sai sharan <s.sharan2008@gmail.com>
2018-06-02 08:21:38 -0400

    inor fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,11 +37,12 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
     BrowserModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
-      {path:'',             component:HomeComponent},
-      {path:'products',     component:ProductsComponent},
-      {path:'shopping-cart', component:ShoppingCartComponent},
-      {path:'check-out',     component:CheckOutComponent},
-      {path:'order-success  ', component:OrderSuccessComponent},
+      {path:'', component:HomeComponent},
+      {path:'products',component:ProductsComponent},
+      {path:'shopping-cart',component:ShoppingCartComponent},
+      {path:'check-out', component:CheckOutComponent},
+      {path:'my-orders',component:MyOrdersComponent},
+      {path:'order-success',component:OrderSuccessComponent},
       {path:'login' , component:LoginComponent},
       {path:'admin/products', component:AdminProductsComponent},
       {path:'admin/orders', component:AdminOrdersComponent}
